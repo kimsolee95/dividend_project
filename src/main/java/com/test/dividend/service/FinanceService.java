7064233bb5e590d243e1db742e2e5329a069e6fd
kimsolee95 <ahhasolee@naver.com>
2023-01-19 11:40:02 +0900

    회사명으로 해당 회사 배당금 정보 조회하는 컨트롤러 추가

--- src/main/java/com/test/dividend/service/FinanceService.java
+++ src/main/java/com/test/dividend/service/FinanceService.java
@@ -1,6 +1,16 @@
 package com.test.dividend.service;
 
+import com.test.dividend.model.Company;
+import com.test.dividend.model.Dividend;
 import com.test.dividend.model.ScrapedResult;
+import com.test.dividend.persist.entity.CompanyEntity;
+import com.test.dividend.persist.entity.DividendEntity;
+import com.test.dividend.persist.repository.CompanyRepository;
+import com.test.dividend.persist.repository.DividendRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +18,40 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class FinanceService {
 
+  private final CompanyRepository companyRepository;
+  private final DividendRepository dividendRepository;
+
   public ScrapedResult getDividendByCompanyName(String companyName) {
-    return null;
+
+    //1. 회사명 기준으로 회사 정보 조회
+    CompanyEntity company = this.companyRepository.findByName(companyName)
+        .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다."));
+
+    //2. 조회된 회사 id로 배당금 조회
+    List<DividendEntity> dividendEntities =  this.dividendRepository.findAllByCompanyId(company.getId());
+
+    //3. 결과 조합후 반환
+//    List<Dividend> dividends = new ArrayList<>();
+//    for (var entity : dividendEntities) {
+//
+//      dividends.add(Dividend.builder()
+//              .date(entity.getDate())
+//              .dividend(entity.getDividend())
+//              .build());
+//    }
+
+    List<Dividend> dividends = dividendEntities.stream()
+                                                .map(e -> Dividend.builder()
+                                                    .date(e.getDate())
+                                                    .dividend(e.getDividend())
+                                                    .build())
+                                                .collect(Collectors.toList());
+
+    return new ScrapedResult(Company.builder()
+                                    .ticker(company.getTicker())
+                                    .name(company.getName())
+                                    .build(),
+                            dividends);
   }
 
 }
--- src/main/java/com/test/dividend/web/FinanceController.java
+++ src/main/java/com/test/dividend/web/FinanceController.java
@@ -1,5 +1,7 @@
 package com.test.dividend.web;
 
+import com.test.dividend.service.FinanceService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/finance")
+@AllArgsConstructor
 public class FinanceController {
+
+  private final FinanceService financeService;
 
   @GetMapping("/dividend/{companyName}")
   public ResponseEntity<?> searchFinance(@PathVariable String companyName) {
 
-    return null;
+    var result = this.financeService.getDividendByCompanyName(companyName);
+    return ResponseEntity.ok(result);
   }
 }
